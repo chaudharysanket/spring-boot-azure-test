470bb172f1bccef6ff9df14e0491d62b2553afd5
Sanket Chaudhary <65975945+chaudharysanket@users.noreply.github.com>
2023-04-09 16:58:56 +0530

    Add files via upload

---
+++ Spring-Boot-Azure-Test/src/main/java/com/sbat/SpringBootAzureTestApplication.java
@@ -0,0 +1,13 @@
+package com.sbat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureTestApplication.class, args);
+	}
+
+}
