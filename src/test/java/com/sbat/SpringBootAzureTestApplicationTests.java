af6591e28e06e1f02c5474e37e51c5128c2e9f55
Sanket Chaudhary <65975945+chaudharysanket@users.noreply.github.com>
2023-04-09 19:42:43 +0530

    Add files via upload

---
+++ src/test/java/com/sbat/SpringBootAzureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzureTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
